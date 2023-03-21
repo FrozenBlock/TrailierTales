751a75f1cef57de3a811548e4e7df6265dbff742
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-03-20 21:34:50 -0400

    Revert "Revert "Revert "bruh extender"""
    This reverts commit eeb49cd05c21725747f964d057b8bd81964616e3.

---
+++ src/main/java/net/lunade/onetwenty/interfaces/SuspiciousSandBlockEntityInterface.java
@@ -0,0 +1,19 @@
+package net.lunade.onetwenty.interfaces;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
+
+public interface SuspiciousSandBlockEntityInterface {
+
+	void luna120$tick();
+
+	boolean luna120$setItem(ItemStack itemStack);
+
+	float luna120$getXOffset(float partialTicks);
+	float luna120$getYOffset(float partialTicks);
+	float luna120$getZOffset(float partialTicks);
+	float luna120$getRotation(float partialTicks);
+
+	Direction luna120$getHitDirection();
+
+}
