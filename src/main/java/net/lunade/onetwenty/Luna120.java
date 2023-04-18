17cc55bca8476060b19406de77fca0d72dbac931
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-04-18 16:56:42 -0400

    Custom Items in Suspicious Blocks, Suspicious Crafting Recipes, & Suspicious Red Sand

--- src/main/java/net/lunade/onetwenty/Luna120.java
+++ src/main/java/net/lunade/onetwenty/Luna120.java
@@ -1,32 +1,57 @@
 package net.lunade.onetwenty;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.lunade.onetwenty.data.Luna120BiomeTags;
 import net.lunade.onetwenty.util.Luna120SharedConstants;
 import net.lunade.onetwenty.worldgen.Luna120FeatureBootstrap;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BrushableBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.level.material.PushReaction;
 import org.jetbrains.annotations.Nullable;
 
 public class Luna120 implements ModInitializer {
 
+	public static final Block SUSPICIOUS_RED_SAND = new BrushableBlock(Blocks.SAND, BlockBehaviour.Properties.of(Material.SAND, MaterialColor.COLOR_ORANGE).strength(0.25f).sound(SoundType.SUSPICIOUS_SAND).pushReaction(PushReaction.DESTROY), SoundEvents.BRUSH_SAND, SoundEvents.BRUSH_SAND_COMPLETED);
+	public static final BlockItem SUSPICIOUS_RED_SAND_ITEM = new BlockItem(SUSPICIOUS_RED_SAND, new Item.Properties());
 	@Override
 	public void onInitialize() {
 		Luna120SharedConstants.startMeasuring(this);
+
+		Registry.register(BuiltInRegistries.BLOCK, Luna120SharedConstants.id("suspicious_red_sand"), SUSPICIOUS_RED_SAND);
+
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(entries ->
+			entries.addAfter(Items.SUSPICIOUS_SAND,  List.of(new ItemStack(SUSPICIOUS_RED_SAND_ITEM)), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS));
 
 		/*
 		BiomeModifications.addFeature(BiomeSelectors.tag(Luna120BiomeTags.HAS_TORCHFLOWER),
--- src/main/java/net/lunade/onetwenty/mixin/BrushableBlockMixin.java
+++ src/main/java/net/lunade/onetwenty/mixin/BrushableBlockMixin.java
@@ -1,13 +1,23 @@
 package net.lunade.onetwenty.mixin;
 
 import net.lunade.onetwenty.interfaces.BrushableBlockEntityInterface;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.monster.piglin.PiglinAi;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.BrushableBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.BrushableBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -16,6 +26,20 @@ public abstract class BrushableBlockMixin extends BaseEntityBlock {
 
 	protected BrushableBlockMixin(Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+		if (level.getBlockEntity(blockPos) instanceof BrushableBlockEntity brushableBlockEntity) {
+			if (brushableBlockEntity.getItem() == ItemStack.EMPTY && brushableBlockEntity.lootTable == null) {
+				ItemStack playerStack = player.getItemInHand(interactionHand).split(1);
+				if (playerStack.getCount() > 0 && playerStack != ItemStack.EMPTY && playerStack.getItem() != Items.AIR) {
+					((BrushableBlockEntityInterface) brushableBlockEntity).luna120$setItem(playerStack);
+					return InteractionResult.SUCCESS;
+				}
+			}
+		}
+		return InteractionResult.PASS;
 	}
 
 	@Nullable
