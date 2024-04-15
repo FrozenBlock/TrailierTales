895b9eb7ebbae68eeded511fb63b06454ca7036b
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-04-15 13:08:26 -0400

    add cyan rose to plant seed advancement

--- src/main/java/net/frozenblock/trailiertales/TrailierTales.java
+++ src/main/java/net/frozenblock/trailiertales/TrailierTales.java
@@ -3,6 +3,8 @@ package net.frozenblock.trailiertales;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.loader.api.ModContainer;
+import net.frozenblock.lib.advancement.api.AdvancementAPI;
+import net.frozenblock.lib.advancement.api.AdvancementEvents;
 import net.frozenblock.lib.entrypoint.api.FrozenModInitializer;
 import net.frozenblock.trailiertales.registry.RegisterBlocks;
 import net.frozenblock.trailiertales.registry.RegisterFeatures;
@@ -13,7 +15,11 @@ import net.frozenblock.trailiertales.registry.RegisterStructures;
 import net.frozenblock.trailiertales.worldgen.TrailierBiomeModifications;
 import net.frozenblock.trailiertales.worldgen.impl.suspicious_handler.SuspiciousData;
 import net.frozenblock.trailiertales.worldgen.impl.suspicious_handler.SuspiciousDataStorage;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.advancements.critereon.ItemUsedOnLocationTrigger;
 import net.minecraft.world.level.storage.DimensionDataStorage;
+import java.util.List;
 
 public class TrailierTales extends FrozenModInitializer {
 
@@ -45,6 +51,27 @@ public class TrailierTales extends FrozenModInitializer {
 
 		ServerTickEvents.END_WORLD_TICK.register(level -> {
 			SuspiciousData.getSuspiciousData(level).swapLists();
+		});
+
+		AdvancementEvents.INIT.register((holder, registries) -> {
+			Advancement advancement = holder.value();
+			//if (AmbienceAndMiscConfig.get().modifyAdvancements) {
+				switch (holder.id().toString()) {
+					case "minecraft:adventure/plant_any_sniffer_seed" -> {
+						AdvancementAPI.addCriteria(advancement, "trailiertales:cyan_rose", CriteriaTriggers.PLACED_BLOCK.createCriterion(
+							ItemUsedOnLocationTrigger.TriggerInstance.placedBlock(RegisterBlocks.CYAN_ROSE_CROP).triggerInstance())
+						);
+						AdvancementAPI.addRequirementsToList(advancement,
+							List.of(
+								"trailiertales:cyan_rose"
+							)
+						);
+					}
+					default -> {
+					}
+				}
+
+			//}
 		});
 
 		TrailierTalesSharedConstants.stopMeasuring(this);
