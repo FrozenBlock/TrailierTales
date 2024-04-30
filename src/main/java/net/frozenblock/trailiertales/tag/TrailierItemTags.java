f184b1c05b9a1fa3834cbf1a384e44e05db71262
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-04-29 21:26:56 -0400

    New Granite, Diorite, and Andesite blocks + Textures

--- src/main/java/net/frozenblock/trailiertales/tag/TrailierItemTags.java
+++ src/main/java/net/frozenblock/trailiertales/tag/TrailierItemTags.java
@@ -10,9 +10,6 @@ public class TrailierItemTags {
 
 	public static final TagKey<Item> COPYABLE_SHERDS = bind("copyable_sherds");
 	public static final TagKey<Item> POT_BASES = bind("pot_bases");
-	public static final TagKey<Item> ANDESITE_BRICKS = bind("andesite_bricks");
-	public static final TagKey<Item> DIORITE_BRICKS = bind("diorite_bricks");
-	public static final TagKey<Item> GRANITE_BRICKS = bind("granite_bricks");
 
 	@NotNull
 	private static TagKey<Item> bind(@NotNull String path) {
