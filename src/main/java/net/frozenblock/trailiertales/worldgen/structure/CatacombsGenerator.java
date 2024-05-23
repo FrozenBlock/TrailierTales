61772c6f89d4fea0b1681a4f4e93682296bb7856
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-05-23 03:06:44 -0400

    h

--- src/main/java/net/frozenblock/trailiertales/worldgen/structure/CatacombsGenerator.java
+++ src/main/java/net/frozenblock/trailiertales/worldgen/structure/CatacombsGenerator.java
@@ -159,7 +159,7 @@ public class CatacombsGenerator {
 			new StructureTemplatePool(
 				empty,
 				List.of(
-					Pair.of(StructurePoolElement.single(string("tomb/decoration/blank"), catacombsDegradation), 55),
+					Pair.of(StructurePoolElement.single(string("tomb/decoration/blank"), catacombsDegradation), 35),
 					Pair.of(StructurePoolElement.single(string("tomb/decoration/soul_lantern"), catacombsDegradation), 15),
 					Pair.of(StructurePoolElement.single(string("tomb/decoration/skull"), catacombsDegradation), 25),
 					Pair.of(StructurePoolElement.single(string("tomb/decoration/skull1"), catacombsDegradation), 12),
@@ -179,11 +179,11 @@ public class CatacombsGenerator {
 				empty,
 				List.of(
 					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_blank"), catacombsDegradation), 150),
-					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_1"), catacombsDegradation), 7),
-					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_2"), catacombsDegradation), 7),
-					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_3"), catacombsDegradation), 7),
-					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_4"), catacombsDegradation), 7),
-					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_5"), catacombsDegradation), 7)
+					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_1"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_2"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_3"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_4"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("decoration/chain/chain_5"), catacombsDegradation), 2)
 				),
 				StructureTemplatePool.Projection.RIGID
 			)
