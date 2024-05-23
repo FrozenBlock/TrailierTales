45c23937b4fa417d951108f01953846a9e03e014
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-05-23 02:42:13 -0400

    ugh

--- src/main/java/net/frozenblock/trailiertales/worldgen/structure/CatacombsGenerator.java
+++ src/main/java/net/frozenblock/trailiertales/worldgen/structure/CatacombsGenerator.java
@@ -159,7 +159,7 @@ public class CatacombsGenerator {
 			new StructureTemplatePool(
 				empty,
 				List.of(
-					Pair.of(StructurePoolElement.single(string("tomb/decoration/blank"), catacombsDegradation), 150),
+					Pair.of(StructurePoolElement.single(string("tomb/decoration/blank"), catacombsDegradation), 80),
 					Pair.of(StructurePoolElement.single(string("tomb/decoration/soul_lantern"), catacombsDegradation), 15),
 					Pair.of(StructurePoolElement.single(string("tomb/decoration/skull"), catacombsDegradation), 25),
 					Pair.of(StructurePoolElement.single(string("tomb/decoration/skull1"), catacombsDegradation), 12),
@@ -178,7 +178,7 @@ public class CatacombsGenerator {
 			new StructureTemplatePool(
 				empty,
 				List.of(
-					Pair.of(StructurePoolElement.single(string("decoration/chain_blank"), catacombsDegradation), 80),
+					Pair.of(StructurePoolElement.single(string("decoration/chain_blank"), catacombsDegradation), 60),
 					Pair.of(StructurePoolElement.single(string("decoration/chain_1"), catacombsDegradation), 7),
 					Pair.of(StructurePoolElement.single(string("decoration/chain_2"), catacombsDegradation), 7),
 					Pair.of(StructurePoolElement.single(string("decoration/chain_3"), catacombsDegradation), 7),
