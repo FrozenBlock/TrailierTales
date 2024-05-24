0d4a7f8e4f2d37bfc258084806708eaa365778c9
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-05-23 21:57:02 -0400

    Add Two More Of Wiggle's Rooms

--- src/main/java/net/frozenblock/trailiertales/worldgen/structure/CatacombsGenerator.java
+++ src/main/java/net/frozenblock/trailiertales/worldgen/structure/CatacombsGenerator.java
@@ -128,9 +128,12 @@ public class CatacombsGenerator {
 					Pair.of(StructurePoolElement.single(string("corridor/dead_end/cap1"), catacombsDegradation), 3), // Total 4
 					Pair.of(StructurePoolElement.single(string("corridor/dead_end/cap_cobwebs"), catacombsDegradation), 1),
 
-					Pair.of(StructurePoolElement.single(string("tomb/two_coffins1"), catacombsDegradation), 7),
-					Pair.of(StructurePoolElement.single(string("tomb/caged_chest"), catacombsDegradation), 7),
-					Pair.of(StructurePoolElement.single(string("tomb/two_chest_doors"), catacombsDegradation), 7)
+					Pair.of(StructurePoolElement.single(string("tomb/two_coffins1"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("tomb/caged_chest"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("tomb/two_chest_doors"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("tomb/small_fancy_tomb"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("tomb/skull_left_chest"), catacombsDegradation), 2),
+					Pair.of(StructurePoolElement.single(string("tomb/skull_right_chest"), catacombsDegradation), 2)
 				),
 				StructureTemplatePool.Projection.RIGID
 			)
