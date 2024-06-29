5e325801bdde7a057c7168c5518f342e771c8a82
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-06-29 13:34:21 -0400

    dont give multiple bruh

--- src/datagen/java/net/frozenblock/trailiertales/datagen/loot/TTArchaeologyLootProvider.java
+++ src/datagen/java/net/frozenblock/trailiertales/datagen/loot/TTArchaeologyLootProvider.java
@@ -11,15 +11,9 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.entries.EmptyLootItem;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.EnchantRandomlyFunction;
-import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
-import net.minecraft.world.level.storage.loot.functions.SetItemDamageFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
-import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import org.jetbrains.annotations.NotNull;
 
 public class TTArchaeologyLootProvider extends SimpleFabricLootTableProvider {
@@ -45,13 +39,13 @@ public class TTArchaeologyLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.ARCHER_POTTERY_SHERD).setWeight(5))
 						.add(LootItem.lootTableItem(Items.ARMS_UP_POTTERY_SHERD).setWeight(5))
 						.add(LootItem.lootTableItem(Items.BLADE_POTTERY_SHERD).setWeight(5))
-						.add(LootItem.lootTableItem(Items.SOUL_LANTERN).setWeight(10).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
-						.add(LootItem.lootTableItem(Items.SKELETON_SKULL).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
-						.add(LootItem.lootTableItem(Items.BONE).setWeight(40).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 7))))
-						.add(LootItem.lootTableItem(Items.ROTTEN_FLESH).setWeight(35).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 5))))
-						.add(LootItem.lootTableItem(Items.COAL).setWeight(20).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 4))))
-						.add(LootItem.lootTableItem(Items.BOOK).setWeight(10)).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3)))
-						.add(LootItem.lootTableItem(Items.CANDLE).setWeight(20).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
+						.add(LootItem.lootTableItem(Items.SOUL_LANTERN).setWeight(10))
+						.add(LootItem.lootTableItem(Items.SKELETON_SKULL).setWeight(2))
+						.add(LootItem.lootTableItem(Items.BONE).setWeight(40))
+						.add(LootItem.lootTableItem(Items.ROTTEN_FLESH).setWeight(35))
+						.add(LootItem.lootTableItem(Items.COAL).setWeight(20))
+						.add(LootItem.lootTableItem(Items.BOOK).setWeight(10))
+						.add(LootItem.lootTableItem(Items.CANDLE).setWeight(20))
 				)
 		);
 
@@ -68,13 +62,13 @@ public class TTArchaeologyLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.ARMS_UP_POTTERY_SHERD).setWeight(5))
 						.add(LootItem.lootTableItem(Items.BLADE_POTTERY_SHERD).setWeight(5))
 						.add(LootItem.lootTableItem(Items.BOOK).setWeight(2).apply(EnchantRandomlyFunction.randomApplicableEnchantment(registries)))
-						.add(LootItem.lootTableItem(Items.SOUL_LANTERN).setWeight(5).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
-						.add(LootItem.lootTableItem(Items.SKELETON_SKULL).setWeight(1).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
-						.add(LootItem.lootTableItem(Items.BONE).setWeight(8).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 7))))
-						.add(LootItem.lootTableItem(Items.ROTTEN_FLESH).setWeight(8).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 5))))
-						.add(LootItem.lootTableItem(Items.COAL).setWeight(7).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 4))))
-						.add(LootItem.lootTableItem(Items.BOOK).setWeight(5)).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3)))
-						.add(LootItem.lootTableItem(Items.CANDLE).setWeight(8).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
+						.add(LootItem.lootTableItem(Items.SOUL_LANTERN).setWeight(5))
+						.add(LootItem.lootTableItem(Items.SKELETON_SKULL).setWeight(1))
+						.add(LootItem.lootTableItem(Items.BONE).setWeight(8))
+						.add(LootItem.lootTableItem(Items.ROTTEN_FLESH).setWeight(8))
+						.add(LootItem.lootTableItem(Items.COAL).setWeight(7))
+						.add(LootItem.lootTableItem(Items.BOOK).setWeight(5))
+						.add(LootItem.lootTableItem(Items.CANDLE).setWeight(8))
 				)
 		);
 
@@ -91,13 +85,13 @@ public class TTArchaeologyLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.ARMS_UP_POTTERY_SHERD).setWeight(2))
 						.add(LootItem.lootTableItem(Items.BLADE_POTTERY_SHERD).setWeight(2))
 						.add(LootItem.lootTableItem(Items.BOOK).setWeight(1).apply(EnchantRandomlyFunction.randomApplicableEnchantment(registries)))
-						.add(LootItem.lootTableItem(Items.SOUL_LANTERN).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
-						.add(LootItem.lootTableItem(Items.SKELETON_SKULL).setWeight(1).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
-						.add(LootItem.lootTableItem(Items.BONE).setWeight(20).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 7))))
-						.add(LootItem.lootTableItem(Items.ROTTEN_FLESH).setWeight(18).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 5))))
-						.add(LootItem.lootTableItem(Items.COAL).setWeight(7).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 4))))
-						.add(LootItem.lootTableItem(Items.BOOK).setWeight(5)).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3)))
-						.add(LootItem.lootTableItem(Items.CANDLE).setWeight(10).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
+						.add(LootItem.lootTableItem(Items.SOUL_LANTERN).setWeight(3))
+						.add(LootItem.lootTableItem(Items.SKELETON_SKULL).setWeight(1))
+						.add(LootItem.lootTableItem(Items.BONE).setWeight(20))
+						.add(LootItem.lootTableItem(Items.ROTTEN_FLESH).setWeight(18))
+						.add(LootItem.lootTableItem(Items.COAL).setWeight(7))
+						.add(LootItem.lootTableItem(Items.BOOK).setWeight(5))
+						.add(LootItem.lootTableItem(Items.CANDLE).setWeight(10))
 				)
 		);
 	}
