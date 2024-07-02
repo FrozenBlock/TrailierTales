e06c4eda9d57f8105b60915f399e53fc08713029
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-07-02 00:18:57 -0400

    this is as much as i can optimize BeardifierMixin

--- src/main/java/net/frozenblock/trailiertales/mixin/common/terrain_adjustment/BeardifierMixin.java
+++ src/main/java/net/frozenblock/trailiertales/mixin/common/terrain_adjustment/BeardifierMixin.java
@@ -3,18 +3,26 @@ package net.frozenblock.trailiertales.mixin.common.terrain_adjustment;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import it.unimi.dsi.fastutil.objects.ObjectListIterator;
 import net.frozenblock.trailiertales.worldgen.TrailierTerrainAdjustment;
-import net.minecraft.util.Mth;
 import net.minecraft.world.level.levelgen.Beardifier;
 import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
+import net.minecraft.world.level.levelgen.structure.pools.JigsawJunction;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Beardifier.class)
 public class BeardifierMixin {
 
+	@Shadow
+	@Final
+	private ObjectListIterator<Beardifier.Rigid> pieceIterator;
 	@Unique
 	@Nullable
 	private TerrainAdjustment trailierTales$terrainAdjustment = null;
@@ -28,27 +36,30 @@ public class BeardifierMixin {
 	@Unique
 	private double trailierTales$zScale = 1D;
 
-	@ModifyExpressionValue(
-		method = "compute",
-		at = @At(
-			value = "INVOKE",
-			target = "Lit/unimi/dsi/fastutil/objects/ObjectListIterator;next()Ljava/lang/Object;",
-			ordinal = 0
-		)
-	)
-	public Object trailierTales$setTerrainAdjustmentValues(Object original) {
-		if (this.trailierTales$terrainAdjustment == null) {
-			if (original instanceof Beardifier.Rigid rigid) {
-				TerrainAdjustment adjustment = rigid.terrainAdjustment();
-				this.trailierTales$terrainAdjustment = adjustment;
-				if (adjustment == TrailierTerrainAdjustment.SMALL_PLATFORM) {
-					this.trailierTales$yOffset = -4;
-					this.trailierTales$xScale = 0.5D;
-					this.trailierTales$zScale = 0.5D;
-				}
+	@Unique
+	private double trailierTales$xContributionScale = 1D;
+
+	@Unique
+	private double trailierTales$yContributionScale = 1D;
+
+	@Unique
+	private double trailierTales$zContributionScale = 1D;
+
+	@Inject(method = "<init>", at = @At("TAIL"))
+	public void trailierTales$setTerrainAdjustmentValues(ObjectListIterator<Beardifier.Rigid> pieceIterator, ObjectListIterator<JigsawJunction> junctionIterator, CallbackInfo info) {
+		if (this.pieceIterator.hasNext()) {
+			TerrainAdjustment adjustment = this.pieceIterator.next().terrainAdjustment();
+			this.trailierTales$terrainAdjustment = adjustment;
+			if (adjustment == TrailierTerrainAdjustment.SMALL_PLATFORM) {
+				this.trailierTales$yOffset = -4;
+				this.trailierTales$xScale = 0.5D;
+				this.trailierTales$zScale = 0.5D;
+				this.trailierTales$xContributionScale = 4D;
+				this.trailierTales$yContributionScale = 3D;
+				this.trailierTales$zContributionScale = 4D;
 			}
+			this.pieceIterator.previous();
 		}
-		return original;
 	}
 
 	@ModifyExpressionValue(
@@ -115,21 +126,11 @@ public class BeardifierMixin {
 		method = "compute",
 		at = @At(
 			value = "INVOKE",
-			target = "Lnet/minecraft/world/level/levelgen/Beardifier;getBuryContribution(DDD)D",
-			ordinal = 0
+			target = "Lnet/minecraft/world/level/levelgen/Beardifier;getBuryContribution(DDD)D"
 		)
 	)
 	public double trailierTales$smallPlatformLogicInBury(double x, double y, double z, Operation<Double> operation) {
-		if (this.trailierTales$terrainAdjustment == TrailierTerrainAdjustment.SMALL_PLATFORM) {
-			return trailierTales$getSmallPlatformContribution(x, y, z);
-		}
-		return operation.call(x, y, z);
-	}
-
-	@Unique
-	private static double trailierTales$getSmallPlatformContribution(double x, double y, double z) {
-		double d = Mth.length(x * 4D, y * 3D, z * 4D);
-		return Mth.clampedMap(d, 0D, 6D, 1D, 0D);
+		return operation.call(x * this.trailierTales$xContributionScale, y * this.trailierTales$yContributionScale, z * this.trailierTales$zContributionScale);
 	}
 
 }
