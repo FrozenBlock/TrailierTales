4e5a1edac2771afd270f959edcf464681a5d5ea9
Treetrain1 <treetrain1@gmail.com>
2024-07-02 22:57:27 -0500

    Add flying to ghosts

--- src/main/java/net/frozenblock/trailiertales/entity/Ghost.java
+++ src/main/java/net/frozenblock/trailiertales/entity/Ghost.java
@@ -7,6 +7,10 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.control.FlyingMoveControl;
+import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
@@ -16,11 +20,34 @@ public class Ghost extends Monster {
 
 	public Ghost(EntityType<? extends Ghost> entityType, Level world) {
 		super(entityType, world);
+		this.moveControl = new FlyingMoveControl(this, 20, true);
 	}
 
 	@NotNull
 	public static AttributeSupplier.Builder createGhostAttributes() {
-		return Mob.createMobAttributes();
+		return Mob.createMobAttributes()
+			.add(Attributes.MAX_HEALTH, 10.0)
+			.add(Attributes.FLYING_SPEED, 0.1F)
+			.add(Attributes.MOVEMENT_SPEED, 0.1F)
+			.add(Attributes.ATTACK_DAMAGE, 3.0);
+	}
+
+	@Override
+	@NotNull
+	protected PathNavigation createNavigation(Level level) {
+		FlyingPathNavigation flyingPathNavigation = new FlyingPathNavigation(this, level);
+		flyingPathNavigation.setCanFloat(true);
+		flyingPathNavigation.setCanOpenDoors(false);
+		flyingPathNavigation.setCanPassDoors(true);
+		return flyingPathNavigation;
+	}
+
+	@Override
+	public void tick() {
+		this.noPhysics = true;
+		super.tick();
+		this.noPhysics = false;
+		this.setNoGravity(true);
 	}
 
 	@Override
