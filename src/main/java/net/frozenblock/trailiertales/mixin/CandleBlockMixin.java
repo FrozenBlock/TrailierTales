8e31e77f654f8c325f60d3dd22cd2c8822a85df4
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-07-25 22:44:19 -0400

    Revert "Update CandleBlockMixin.java"
    This reverts commit 9b7f5450aa33c98e5200267a888e3d63eb255dd2.

--- src/main/java/net/frozenblock/trailiertales/mixin/CandleBlockMixin.java
+++ src/main/java/net/frozenblock/trailiertales/mixin/CandleBlockMixin.java
@@ -6,6 +6,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import static com.sk89q.worldedit.world.item.ItemCategories.CANDLES;
+import static net.frozenblock.lib.block.api.MultifaceClusterBlock.WATERLOGGED;
+import static net.minecraft.world.level.block.CopperBulbBlock.LIT;
 
 @Mixin(CandleBlock.class)
 public class CandleBlockMixin {
@@ -13,7 +16,7 @@ public class CandleBlockMixin {
 	@Inject(method = "<init>", at = @At("TAIL"))
 	public void trailierTales$boostUpUgh(BlockBehaviour.Properties settings, CallbackInfo ci) {
 		CandleBlock.class.cast(this).registerDefaultState(
-			CandleBlock.class.cast(this).getStateDefinition().any().setValue(CandleBlock.CANDLES, 4).setValue(CandleBlock.LIT, false).setValue(CandleBlock.WATERLOGGED, false)
+			CandleBlock.class.cast(this).any().setValue(CANDLES, 4).setValue(LIT, false).setValue(WATERLOGGED, false)
 		);
 	}
 }
