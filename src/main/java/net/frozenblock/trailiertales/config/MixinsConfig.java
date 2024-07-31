b2d16f7469b4e98967bf4de27577337c66cd93c9
Treetrain1 <treetrain1@gmail.com>
2024-07-31 17:26:19 -0500

    Add Mixins JSON5 Config

---
+++ src/main/java/net/frozenblock/trailiertales/config/MixinsConfig.java
@@ -0,0 +1,45 @@
+package net.frozenblock.trailiertales.config;
+
+import net.frozenblock.lib.config.api.instance.Config;
+import net.frozenblock.lib.config.api.instance.json.JsonConfig;
+import net.frozenblock.lib.config.api.instance.json.JsonType;
+import net.frozenblock.lib.config.api.registry.ConfigRegistry;
+import net.frozenblock.lib.shadow.blue.endless.jankson.Comment;
+import org.jetbrains.annotations.ApiStatus;
+import static net.frozenblock.trailiertales.TrailierConstants.MOD_ID;
+import static net.frozenblock.trailiertales.TrailierConstants.configPath;
+
+@ApiStatus.Internal
+public final class MixinsConfig {
+
+	public static final Config<MixinsConfig> INSTANCE = ConfigRegistry.register(
+		new JsonConfig<>(
+			MOD_ID,
+			MixinsConfig.class,
+			configPath("mixins", true),
+			JsonType.JSON5,
+			false,
+			null,
+			null
+		)
+	);
+
+	public boolean brush = true;
+
+	public boolean brushable_block = true;
+
+	public boolean coffin = true;
+
+	public boolean decorated_pot = true;
+
+	@Comment("Client only")
+	public boolean haunt = true;
+
+	public boolean surveyor = true;
+
+	public boolean terrain_adjustment = true;
+
+	public static MixinsConfig get() {
+		return INSTANCE.config();
+	}
+}
---
+++ src/main/java/net/frozenblock/trailiertales/mixin/TTMixinPlugin.java
@@ -0,0 +1,47 @@
+package net.frozenblock.trailiertales.mixin;
+
+import net.frozenblock.trailiertales.config.MixinsConfig;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import java.util.List;
+import java.util.Set;
+
+public final class TTMixinPlugin implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		MixinsConfig config = MixinsConfig.get();
+
+		if (mixinClassName.contains("brush")) return config.brush;
+		if (mixinClassName.contains("brushable_block")) return config.brushable_block;
+		if (mixinClassName.contains("coffin")) return config.coffin;
+		if (mixinClassName.contains("decorated_pot")) return config.decorated_pot;
+		if (mixinClassName.contains("haunt")) return config.haunt;
+		if (mixinClassName.contains("surveyor")) return config.surveyor;
+		if (mixinClassName.contains("terrain_adjustment")) return config.terrain_adjustment;
+
+		return true;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+}
