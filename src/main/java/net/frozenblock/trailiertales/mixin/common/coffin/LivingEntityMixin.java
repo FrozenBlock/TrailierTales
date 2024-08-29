78f83fd4bc1bb5b24ac3379c696da82a694c6f07
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-08-29 14:59:59 -0400

    test interaction times

--- src/main/java/net/frozenblock/trailiertales/mixin/common/coffin/LivingEntityMixin.java
+++ src/main/java/net/frozenblock/trailiertales/mixin/common/coffin/LivingEntityMixin.java
@@ -1,14 +1,20 @@
 package net.frozenblock.trailiertales.mixin.common.coffin;
 
 import java.util.Optional;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.frozenblock.trailiertales.block.entity.coffin.CoffinSpawner;
 import net.frozenblock.trailiertales.block.entity.coffin.CoffinSpawnerData;
 import net.frozenblock.trailiertales.block.entity.coffin.impl.EntityCoffinData;
 import net.frozenblock.trailiertales.block.entity.coffin.impl.EntityCoffinInterface;
 import net.frozenblock.trailiertales.registry.RegisterParticles;
+import net.minecraft.advancements.critereon.EntityHurtPlayerTrigger;
+import net.minecraft.advancements.critereon.PlayerHurtEntityTrigger;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
@@ -42,6 +48,38 @@ public abstract class LivingEntityMixin implements EntityCoffinInterface {
 	@Override
 	public void trailierTales$setCoffinData(EntityCoffinData entityCoffinData) {
 		this.trailierTales$entityCoffinData = entityCoffinData;
+	}
+
+	@WrapOperation(
+		method = "hurt",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/advancements/critereon/PlayerHurtEntityTrigger;trigger(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/damagesource/DamageSource;FFZ)V"
+		)
+	)
+	public void trailierTales$onHurtByPlayer(
+		PlayerHurtEntityTrigger instance, ServerPlayer player, Entity entity, DamageSource damage, float dealt, float taken, boolean blocked, Operation<Void> original
+	) {
+		if (this.trailierTales$entityCoffinData != null) {
+			this.trailierTales$entityCoffinData.updateLastInteraction(entity.level().getGameTime());
+		}
+		original.call(instance, player, entity, damage, dealt, taken, blocked);
+	}
+
+	@WrapOperation(
+		method = "hurt",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/advancements/critereon/EntityHurtPlayerTrigger;trigger(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/damagesource/DamageSource;FFZ)V"
+		)
+	)
+	public void trailierTales$onHurtPlayer(
+		EntityHurtPlayerTrigger instance, ServerPlayer player, DamageSource source, float dealt, float taken, boolean blocked, Operation<Void> original
+	) {
+		if (this.trailierTales$entityCoffinData != null) {
+			this.trailierTales$entityCoffinData.updateLastInteraction(LivingEntity.class.cast(this).level().getGameTime());
+		}
+		original.call(instance, player, source, dealt, taken, blocked);
 	}
 
 	@Inject(method = "remove", at = @At("HEAD"))
