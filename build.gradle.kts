8b016e7942ea81657ecb3aaa31be76d4e41501fb
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-09-27 22:39:03 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -204,7 +204,7 @@ dependencies {
 
     // Wilder Wild
     if (local_wilderwild)
-        modCompileOnlyApi(project(":WilderWild", configuration = "namedElements"))
+        api(project(":WilderWild", configuration = "namedElements"))
     else
         modCompileOnly("maven.modrinth:wilder-wild:$wilderwild_version")
 
