833a025c0401a324af79e19805d75dd82bb70771
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-09-29 03:08:05 -0400

    huge W

--- src/main/java/net/frozenblock/trailiertales/TTPreLoadConstants.java
+++ src/main/java/net/frozenblock/trailiertales/TTPreLoadConstants.java
@@ -2,27 +2,14 @@
 package net.frozenblock.trailiertales;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.frozenblock.lib.config.api.instance.ConfigModification;
-import net.frozenblock.lib.config.api.registry.ConfigRegistry;
-import net.frozenblock.wilderwild.config.WWBlockConfig;
+import java.util.Arrays;
 
 public class TTPreLoadConstants {
-	public static final boolean IS_DATAGEN;
+	public static final boolean IS_DATAGEN = isDatagen();
 
 	private static boolean isDatagen() {
-		boolean isDatagen = false;
-		try {
-			Class.forName("net.frozenblock.trailiertales.datagen.TTDatagenModule", false, TTPreLoadConstants.class.getClassLoader());
-			isDatagen = true;
-		} catch (ClassNotFoundException ignored) {}
-
-		return isDatagen;
-	}
-
-	static {
-		IS_DATAGEN = isDatagen();
-		if (IS_DATAGEN && FabricLoader.getInstance().isModLoaded("wilderwild")) {
-			ConfigRegistry.register(WWBlockConfig.INSTANCE, new ConfigModification<>(config -> config.snowlogging.snowlogging = false));
-		}
+		return Arrays.stream(
+			FabricLoader.getInstance().getLaunchArguments(true)
+		).toList().stream().anyMatch(string -> string.contains("datagen"));
 	}
 }
