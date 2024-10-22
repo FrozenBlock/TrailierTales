a4abb58a1308ec1e8f5feb82ba12af204f2ca2f1
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-10-22 17:52:49 -0400

    Coffin Mobs Can No Longer Switch Dimensions

--- src/main/java/net/frozenblock/trailiertales/mixin/common/coffin/EntityMixin.java
+++ src/main/java/net/frozenblock/trailiertales/mixin/common/coffin/EntityMixin.java
@@ -1,6 +1,7 @@
 package net.frozenblock.trailiertales.mixin.common.coffin;
 
 import net.frozenblock.trailiertales.block.CoffinBlock;
+import net.frozenblock.trailiertales.block.entity.coffin.impl.EntityCoffinInterface;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.portal.DimensionTransition;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,9 +12,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Entity.class)
 public class EntityMixin {
 
-	@Inject(method = "changeDimension", at = @At(value = "HEAD"))
+	@Inject(method = "changeDimension", at = @At("HEAD"))
 	public void trailierTales$changeDimension(DimensionTransition dimensionTransition, CallbackInfoReturnable<Entity> info) {
 		CoffinBlock.onCoffinUntrack(Entity.class.cast(this), null, true);
+	}
+
+	@Inject(method = "canChangeDimensions", at = @At("HEAD"), cancellable = true)
+	public void trailierTales$canChangeDimensions(CallbackInfoReturnable<Boolean> info) {
+		if (Entity.class.cast(this) instanceof EntityCoffinInterface entityCoffinInterface) {
+			if (entityCoffinInterface.trailierTales$getCoffinData() != null) {
+				info.setReturnValue(false);
+			}
+		}
 	}
 
 }
