85540ac9540aa59ba4135b940664f6939fd556b2
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-12-30 15:03:29 -0500

    help

--- src/main/java/net/frozenblock/trailiertales/registry/TTDecoratedPotPatternRegistry.java
+++ src/main/java/net/frozenblock/trailiertales/registry/TTDecoratedPotPatternRegistry.java
@@ -5,11 +5,50 @@ import net.frozenblock.trailiertales.TTConstants;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.entity.DecoratedPotPattern;
 import org.jetbrains.annotations.NotNull;
+import java.util.Map;
 
 public class TTDecoratedPotPatternRegistry implements DecoratedPotPatternRegistryEntrypoint {
+
+	@Override
+	public void registerForItems(Map<Item, ResourceKey<DecoratedPotPattern>> map) {
+		put(map, TTItems.AURORA_POTTERY_SHERD, "aurora");
+		put(map, TTItems.BAIT_POTTERY_SHERD, "bait");
+		put(map, TTItems.BLOOM_POTTERY_SHERD, "bloom");
+		put(map, TTItems.BOLT_POTTERY_SHERD, "bolt");
+		put(map, TTItems.BULLSEYE_POTTERY_SHERD, "bullseye");
+		put(map, TTItems.CARRIER_POTTERY_SHERD, "carrier");
+		put(map, TTItems.CLUCK_POTTERY_SHERD, "cluck");
+		put(map, TTItems.CRAWL_POTTERY_SHERD, "crawl");
+		put(map, TTItems.CRESCENT_POTTERY_SHERD, "crescent");
+		put(map, TTItems.CULTIVATOR_POTTERY_SHERD, "cultivator");
+		put(map, TTItems.DROUGHT_POTTERY_SHERD, "drought");
+		put(map, TTItems.ENCLOSURE_POTTERY_SHERD, "enclosure");
+		put(map, TTItems.ESSENCE_POTTERY_SHERD, "essence");
+		put(map, TTItems.EYE_POTTERY_SHERD, "eye");
+		put(map, TTItems.FOCUS_POTTERY_SHERD, "focus");
+		put(map, TTItems.FROST_POTTERY_SHERD, "frost");
+		put(map, TTItems.HARE_POTTERY_SHERD, "hare");
+		put(map, TTItems.HEIGHT_POTTERY_SHERD, "height");
+		put(map, TTItems.HUMP_POTTERY_SHERD, "hump");
+		put(map, TTItems.ILLUMINATOR_POTTERY_SHERD, "illuminator");
+		put(map, TTItems.INCIDENCE_POTTERY_SHERD, "incidence");
+		put(map, TTItems.LUMBER_POTTERY_SHERD, "lumber");
+		put(map, TTItems.NAVIGATOR_POTTERY_SHERD, "navigator");
+		put(map, TTItems.NEEDLES_POTTERY_SHERD, "needles");
+		put(map, TTItems.OMEN_POTTERY_SHERD, "omen");
+		put(map, TTItems.PLUME_POTTERY_SHERD, "plume");
+		put(map, TTItems.PROTECTION_POTTERY_SHERD, "protection");
+		put(map, TTItems.SHED_POTTERY_SHERD, "shed");
+		put(map, TTItems.SHINE_POTTERY_SHERD, "shine");
+		put(map, TTItems.SHOWER_POTTERY_SHERD, "shower");
+		put(map, TTItems.SPADE_POTTERY_SHERD, "spade");
+		put(map, TTItems.SPROUT_POTTERY_SHERD, "sprout");
+		put(map, TTItems.VESSEL_POTTERY_SHERD, "vessel");
+		put(map, TTItems.WITHER_POTTERY_SHERD, "wither");
+	}
 
 	@Override
 	public void bootstrap(Registry<DecoratedPotPattern> registry) {
@@ -49,12 +88,15 @@ public class TTDecoratedPotPatternRegistry implements DecoratedPotPatternRegistr
 		register(registry, "wither");
 	}
 
-	public static void register(@NotNull Registry<DecoratedPotPattern> registry, String sherdName) {
-		ResourceLocation location = TTConstants.id(sherdName + "_pottery_pattern");
+	private static void put(@NotNull Map<Item, ResourceKey<DecoratedPotPattern>> map, @NotNull Item sherd, String sherdName) {
+		map.put(sherd, ResourceKey.create(Registries.DECORATED_POT_PATTERN, TTConstants.id(sherdName)));
+	}
+
+	private static void register(@NotNull Registry<DecoratedPotPattern> registry, String sherdName) {
 		DecoratedPotPatternRegistryEntrypoint.register(
 			registry,
-			ResourceKey.create(Registries.DECORATED_POT_PATTERN, location),
-			location
+			ResourceKey.create(Registries.DECORATED_POT_PATTERN, TTConstants.id(sherdName)),
+			TTConstants.id(sherdName + "_pottery_pattern")
 		);
 	}
 
