1e0349e7588dcb6ef0042cf30652cc6504a08c12
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-12-31 02:33:21 -0500

    improve loot tables

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,11 +6,16 @@ hi
 - Increased the protocol version to 2.
 - Added 50 new rooms for Catacombs.
 - Skulls, Bones, and Rotten Flesh are no longer archeology loot in Catacombs.
+- Improved the loot tables for suspicious blocks in all of Trailier Tales' structures, providing more insight into the past.
+- Ominous Bottles can now be found in Catacombs.
 - Mobs spawned from Coffins now drop twice as much XP if killed by a player with Siege Omen.
 - Added the Block of Ectoplasm.
   - While inside this block, gravity is weaker.
   - Apparitions cannot move through these blocks.
 - Added plenty of new large pieces to all Ruins types.
+- - Revamped how Ruins structure pieces are loaded, chosen, and offset during worldgen, being much easier and more automatic.
+- As a side effect of this, all ruins pieces now have an equal chance of generating. Prior to this revamp, groups of pieces with specific offsets had a certain chance of being selected.
+- Generic and Jungle ruins now generate with one more piece on average.
 - Added new ruins for snowy biomes.
 - Added the Embrace Armor Trim.
 - Added the Aurora Pottery Sherd.
@@ -20,9 +25,6 @@ hi
 - Added the Stasis Music Disc.
 - Updated the Undead Armor Trim Smithing Template texture.
 - Updated the Undead Armor Trim's leggings texture.
-- Revamped how Ruins structure pieces are loaded, chosen, and offset during worldgen, being much easier and more automatic.
-  - As a side effect of this, all ruins pieces now have an equal chance of generating. Prior to this revamp, groups of pieces with specific offsets had a certain chance of being selected.
-- Generic and Jungle ruins now generate with one more piece on average.
 - Added compat for Enchantment Descriptions, thanks to Lufurrius! ([#12](https://github.com/FrozenBlock/TrailierTales/pull/12))
 - Added Spanish translations, thanks to Kokoroto!
 - Added Ukranian translations, thanks to unroman! ([#8](https://github.com/FrozenBlock/TrailierTales/pull/8))
--- src/main/java/net/frozenblock/trailiertales/datagen/loot/TTChestLootProvider.java
+++ src/main/java/net/frozenblock/trailiertales/datagen/loot/TTChestLootProvider.java
@@ -9,6 +9,7 @@ import net.frozenblock.trailiertales.registry.TTItems;
 import net.frozenblock.trailiertales.registry.TTLootTables;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.packs.VanillaChestLoot;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
@@ -20,6 +21,7 @@ import net.minecraft.world.level.storage.loot.functions.EnchantRandomlyFunction;
 import net.minecraft.world.level.storage.loot.functions.SetEnchantmentsFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemDamageFunction;
+import net.minecraft.world.level.storage.loot.functions.SetOminousBottleAmplifierFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
@@ -53,6 +55,12 @@ public class TTChestLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.EMERALD).setWeight(8).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.IRON_INGOT).setWeight(4).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.GOLD_INGOT).setWeight(6).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
+						.add(
+							LootItem.lootTableItem(Items.OMINOUS_BOTTLE)
+								.setWeight(1)
+								.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1F)))
+								.apply(SetOminousBottleAmplifierFunction.setAmplifier(UniformGenerator.between(2F, 4F)))
+						)
 						.add(EmptyLootItem.emptyItem().setWeight(3))
 				)
 				.withPool(
@@ -63,6 +71,8 @@ public class TTChestLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.BONE).setWeight(8).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 6))))
 						.add(LootItem.lootTableItem(Items.ROTTEN_FLESH).setWeight(6).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.COAL).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 4))))
+						.add(LootItem.lootTableItem(Items.RAIL).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 4))))
+						.add(LootItem.lootTableItem(Items.CHAIN).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 2))))
 						.add(LootItem.lootTableItem(Items.BOOK).setWeight(4)).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3)))
 						.add(LootItem.lootTableItem(Items.CANDLE).setWeight(5).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.BRICK).setWeight(5).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
@@ -86,7 +96,7 @@ public class TTChestLootProvider extends SimpleFabricLootTableProvider {
 								.setWeight(2).setQuality(Rarity.EPIC.ordinal())
 								.apply(new SetEnchantmentsFunction.Builder().withEnchantment(
 									registries.lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(TTEnchantments.REBRUSH),
-									ConstantValue.exactly(1.0F)
+									ConstantValue.exactly(1F)
 								))
 						)
 						.add(EmptyLootItem.emptyItem().setWeight(3))
@@ -99,6 +109,12 @@ public class TTChestLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.DIAMOND).setWeight(2))
 						.add(LootItem.lootTableItem(Items.NAME_TAG).setWeight(8))
 						.add(LootItem.lootTableItem(Items.EMERALD).setWeight(7).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
+						.add(
+							LootItem.lootTableItem(Items.OMINOUS_BOTTLE)
+								.setWeight(1)
+								.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1F)))
+								.apply(SetOminousBottleAmplifierFunction.setAmplifier(UniformGenerator.between(2F, 4F)))
+						)
 						.add(EmptyLootItem.emptyItem().setWeight(2))
 				)
 				.withPool(
@@ -126,6 +142,8 @@ public class TTChestLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.FLOWER_POT).setWeight(5).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.DEAD_BUSH).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.STICK).setWeight(5).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
+						.add(LootItem.lootTableItem(Items.RAIL).setWeight(1).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 4))))
+						.add(LootItem.lootTableItem(Items.CHAIN).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 2))))
 				)
 		);
 
@@ -203,6 +221,12 @@ public class TTChestLootProvider extends SimpleFabricLootTableProvider {
 						.setRolls(ConstantValue.exactly(1F))
 						.add(LootItem.lootTableItem(TTItems.MATRIX_ARMOR_TRIM_SMITHING_TEMPLATE).setWeight(1))
 						.add(LootItem.lootTableItem(TTItems.UNDEAD_ARMOR_TRIM_SMITHING_TEMPLATE).setWeight(1))
+						.add(
+							LootItem.lootTableItem(Items.OMINOUS_BOTTLE)
+								.setWeight(1)
+								.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1F)))
+								.apply(SetOminousBottleAmplifierFunction.setAmplifier(UniformGenerator.between(2F, 4F)))
+						)
 						.add(EmptyLootItem.emptyItem().setWeight(18))
 				)
 				.withPool(
@@ -231,6 +255,8 @@ public class TTChestLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.FLOWER_POT).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.DEAD_BUSH).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.STICK).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
+						.add(LootItem.lootTableItem(Items.RAIL).setWeight(1).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 4))))
+						.add(LootItem.lootTableItem(Items.CHAIN).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 2))))
 				)
 		);
 
@@ -275,6 +301,7 @@ public class TTChestLootProvider extends SimpleFabricLootTableProvider {
 						.add(LootItem.lootTableItem(Items.BOOK).setWeight(4)).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 4)))
 						.add(LootItem.lootTableItem(Items.DEAD_BUSH).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
 						.add(LootItem.lootTableItem(Items.STICK).setWeight(5).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))))
+						.add(LootItem.lootTableItem(Items.CHAIN).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 2))))
 				)
 		);
 	}
