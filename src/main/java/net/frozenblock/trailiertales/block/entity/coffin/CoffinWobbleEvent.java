ebdb8aa71ee67be20e1d30c4f52a97ac54aa504a
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-02-12 23:17:55 -0500

    less common

--- src/main/java/net/frozenblock/trailiertales/block/entity/coffin/CoffinWobbleEvent.java
+++ src/main/java/net/frozenblock/trailiertales/block/entity/coffin/CoffinWobbleEvent.java
@@ -1,5 +1,8 @@
 package net.frozenblock.trailiertales.block.entity.coffin;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
 import net.frozenblock.lib.FrozenLibConstants;
 import net.frozenblock.trailiertales.block.CoffinBlock;
 import net.frozenblock.trailiertales.registry.TTMobEffects;
@@ -25,9 +28,6 @@ import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.BiFunction;
 
 public enum CoffinWobbleEvent {
 	EJECT_LOOT(0.4F, true, (coffinBlockEntity, blockState) -> !coffinBlockEntity.isEmpty()),
@@ -63,43 +63,50 @@ public enum CoffinWobbleEvent {
 	}
 
 	public static void onWobble(ServerLevel level, BlockPos pos, BlockState state, CoffinBlockEntity coffinBlockEntity, RandomSource random) {
-		for (CoffinWobbleEvent event : Util.shuffledCopy(CoffinWobbleEvent.values(), random)) {
-			if (event.getChance() <= random.nextFloat() && event.checkExtraConditions(coffinBlockEntity, state)) {
+		CoffinWobbleEvent event = Util.getRandom(CoffinWobbleEvent.values(), random);
+		if (event.getChance() <= random.nextFloat() && event.checkExtraConditions(coffinBlockEntity, state)) {
 
-				Vec3 centerPos = CoffinBlock.getCenter(state, pos);;
-				if (event == EJECT_LOOT) {
+			Vec3 centerPos = CoffinBlock.getCenter(state, pos);
+
+			switch (event) {
+				case EJECT_LOOT:
 					ejectRandomItem(level, centerPos, coffinBlockEntity);
-				} else if (event == HAUNT) {
+					break;
+				case HAUNT:
 					applyHaunt(level, centerPos, coffinBlockEntity);
-				} else if (event == ACTIVATE) {
+					break;
+				case ACTIVATE:
 					coffinBlockEntity.getCoffinSpawner().immediatelyActivate(level, pos);
-				} else if (event == MINING_FATIGUE_POTION) {
+					break;
+				case MINING_FATIGUE_POTION:
 					ejectProjectile(level, centerPos, createItemStackForMobEffect(Items.SPLASH_POTION, MINING_FATIGUE));
-				} else if (event == POISON_POTION) {
+					break;
+				case POISON_POTION:
 					ejectProjectile(level, centerPos, createItemStackForMobEffect(Items.SPLASH_POTION, POISON));
-				} else if (event == EXPERIENCE_BOTTLE) {
+					break;
+				case EXPERIENCE_BOTTLE:
 					ejectProjectile(level, centerPos, new ItemStack(Items.EXPERIENCE_BOTTLE));
-				}
-
-				if (event.playsLidAnim()) {
-					coffinBlockEntity.coffinWobbleLidAnimTicks = 4;
-					Direction coffinOrientation = CoffinBlock.getCoffinOrientation(level, pos);
-					if (coffinOrientation != null) {
-						CoffinBlock.spawnParticlesFrom(
-							level,
-							ParticleTypes.DUST_PLUME,
-							level.random.nextInt(8, 14),
-							0.02D,
-							coffinOrientation,
-							pos,
-							0.375D
-						);
-					}
-				}
-
-				coffinBlockEntity.markUpdated();
-				return;
+					break;
+				default: throw new IllegalStateException("Unexpected value: " + event.name());
 			}
+
+			if (event.playsLidAnim()) {
+				coffinBlockEntity.coffinWobbleLidAnimTicks = 4;
+				Direction coffinOrientation = CoffinBlock.getCoffinOrientation(level, pos);
+				if (coffinOrientation != null) {
+					CoffinBlock.spawnParticlesFrom(
+						level,
+						ParticleTypes.DUST_PLUME,
+						level.random.nextInt(8, 14),
+						0.02D,
+						coffinOrientation,
+						pos,
+						0.375D
+					);
+				}
+			}
+
+			coffinBlockEntity.markUpdated();
 		}
 	}
 
