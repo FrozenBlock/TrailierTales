25218605d1b2c6a2293f958bc1f1c01ceba62109
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-09-17 13:22:11 -0400

    Update TTModelLayers.java

--- common/src/main/java/net/frozenblock/trailiertales/client/TTModelLayers.java
+++ common/src/main/java/net/frozenblock/trailiertales/client/TTModelLayers.java
@@ -30,6 +30,7 @@ import net.frozenblock.trailiertales.registry.TTEntityTypes;
 import net.mehvahdjukaar.candlelight.api.ClientOnly;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 
 @ClientOnly
@@ -57,10 +58,8 @@ public final class TTModelLayers {
 	 */
 	public static void setup() {
 		BlockEntityRenderers.register(TTBlockEntityTypes.COFFIN.get(), CoffinRenderer::new);
-
-		EntityRendererRegistry.register(TTEntityTypes.APPARITION.get(), ApparitionRenderer::new);
-
-		EntityRendererRegistry.register(TTEntityTypes.THROWN_ITEM_PROJECTILE.get(), ThrownItemRenderer::new);
+		EntityRenderers.register(TTEntityTypes.APPARITION.get(), ApparitionRenderer::new);
+		EntityRenderers.register(TTEntityTypes.THROWN_ITEM_PROJECTILE.get(), ThrownItemRenderer::new);
 	}
 
 	private TTModelLayers() {}
